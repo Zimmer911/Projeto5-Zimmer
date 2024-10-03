90b47d1fba2f62f1f3922d44efcf65285d7d4dda
Zimmer911 <150195815+Zimmer911@users.noreply.github.com>
2024-10-03 19:29:12 -0300

    Update Telafeed.java

--- Frontend/app/src/main/java/br/com/aula/text/Telafeed.java
+++ Frontend/app/src/main/java/br/com/aula/text/Telafeed.java
@@ -14,6 +14,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Telafeed extends AppCompatActivity {
 
+    private Button btnpostagem;
+    private TextView textfeednome;
+    private TextView textfeedcoment;
+    private TextView textfeedtitulo;
+    private TextView textfeedimage;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_telafeed);
 
+        btnpostagem = findViewById(R.id.btnpostagem);
+        textfeednome = findViewById(R.id.textfeednome);
+        textfeedcoment = findViewById(R.id.textfeedcoment);
+        textfeedtitulo = findViewById(R.id.textfeedtitulo);
+        textfeedimage = findViewById(R.id.textfeedimage);
+
+        btnpostagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Telafeed.this, postagem.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
