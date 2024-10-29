49161b201dceed604750f6bc9b3f9fa3c37dcae4
Zimmer911 <150195815+Zimmer911@users.noreply.github.com>
2024-10-29 12:02:46 -0300

    Criptografia
    Criptografia

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,19 +1,32 @@
+// src/models/User.js
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/config");
 
 const User = sequelize.define('user', {
     nome: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
     },
     email: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
     },
     senha: {
-        type: DataTypes.STRING,
-        allowNull: false
+        type: DataTypes.STRING(88), // Aumentado para acomodar o hash+salt em Base64
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
     }
+}, {
+    timestamps: true // Adiciona created_at e updated_at
 });
 
 module.exports = User;
--- Backend/src/router/routerUser.js
+++ Backend/src/router/routerUser.js
@@ -1,27 +1,37 @@
-const {Router} = require("express");
+// src/router/routerUser.js
+const { Router } = require("express");
 const UserController = require("../controller/UserController");
 const { validateUser, validateUserId } = require("../middlewares/ValidateUser");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 const router = Router();
 
-router.post('/', validateUser ,(req,res) => {
-    UserController.create(req,res)
-});
-router.put('/:id',validateUser,validateUserId,(req,res) => {
-    UserController.update(req,res)
-});
-router.get('/',(req,res) => {
-    UserController.getAll(req,res)
-});
-router.get('/:id',validateUserId,(req,res) => {
-    UserController.getOne(req,res)
-});
-router.delete('/:id',validateUserId,(req,res) => {
-    UserController.delete(req,res)
+// Rotas públicas
+router.post('/register', validateUser, (req, res) => {
+    UserController.create(req, res);
 });
 
 router.post('/login', (req, res) => {
-    UserController.login(req, res)
-  });
-  
+    UserController.login(req, res);
+});
+
+// Rotas protegidas
+router.use(authenticateToken); // Middleware de autenticação para as rotas abaixo
+
+router.put('/:id', validateUser, validateUserId, (req, res) => {
+    UserController.update(req, res);
+});
+
+router.put('/:id/password', validateUserId, (req, res) => {
+    UserController.updatePassword(req, res);
+});
+
+router.get('/profile', (req, res) => {
+    UserController.getProfile(req, res);
+});
+
+router.delete('/:id', validateUserId, (req, res) => {
+    UserController.delete(req, res);
+});
+
 module.exports = router;
